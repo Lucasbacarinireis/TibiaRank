abb1a03d1b4ea85ef9c54587131e7aa4fc119f3e
Lucas Bacarini Reis <lucasbacarinireis1@gmail.com>
2022-08-01 21:45:12 -0300

    adicionado 2 fragments -> Rank e Imbuiment com a lógica do recycler view + adapter + api no fragment Rank

--- app/src/main/java/com/example/tibiarank/MainActivity.kt
+++ app/src/main/java/com/example/tibiarank/MainActivity.kt
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tibiarank.adapter.TibiaRankAdapter
 import com.example.tibiarank.databinding.ActivityMainBinding
+import com.example.tibiarank.fragments.Imbuiment
+import com.example.tibiarank.fragments.Rank
 import com.example.tibiarank.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModdel = MainViewModel()
-    lateinit var recyclerView: RecyclerView
-    val adapterRank = TibiaRankAdapter()
+//    private val viewModdel = MainViewModel()
+//    lateinit var recyclerView: RecyclerView
+//    val adapterRank = TibiaRankAdapter()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,14 +25,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        recyclerView = binding.rvMain
-        recyclerView.adapter = adapterRank
+//        recyclerView = binding.rvMain
+//        recyclerView.adapter = adapterRank
 
-        viewModdel.data.observe(this) { lista ->
-            Log.e("OK", lista.toString())
-            adapterRank.addItems(lista)
+//        viewModdel.data.observe(this) { lista ->
+//            Log.e("OK", lista.toString())
+//            adapterRank.addItems(lista)
+//        }
+//        viewModdel.getAllRank()
+
+        replaceFragment(Rank())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(Rank())
+                R.id.imbuiment -> replaceFragment(Imbuiment())
+
+                else -> {
+
+                }
+            }
+            true
         }
-        viewModdel.getAllRank()
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransition = fragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.frame_layout, fragment)
+        fragmentTransition.commit()
+
 
 
     }
